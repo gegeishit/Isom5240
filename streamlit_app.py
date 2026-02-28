1a9f262669080f80f79acefca648a665cc743773
gegeishit <83584043+gegeishit@users.noreply.github.com>
2026-02-28 17:38:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,32 @@
 from transformers import pipeline
 from PIL import Image
+import streamlit as st
 
-# Streamlit UI
-print("Title: Age Classification using ViT")
+#function part
+def ageClassifier():
+    age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
+    
+    image_name = "middleagedMan.jpg"
+    image_name = Image.open(image_name).convert("RGB")
+    
+    # Classify age
+    age_predictions = age_classifier(image_name)
 
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
+def main():
+    # Streamlit UI
+    st.header("Title: Age Classification using ViT")
+    
+    age_predictions = ageClassifier()
+  
+    st.write(age_predictions)
+    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+    
+    # Display results
+    st.write("Predicted Age Range:")
+    st.write(f"Age range: {age_predictions[0]['label']}")
+    
+    st.write("Done)
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
-
-# Classify age
-age_predictions = age_classifier(image_name)
-print(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-
-# Display results
-print("Predicted Age Range:")
-print(f"Age range: {age_predictions[0]['label']}")
+  
+#main part
+main()
